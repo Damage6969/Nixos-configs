8503cf8a16f60c2034b1820175318b330b9dff01
Damage6969 <j.haines@outlook.com>
2023-03-22 16:08:57 -0400

    after clone

--- modules/networking/default.nix
+++ modules/networking/default.nix
@@ -25,7 +25,7 @@ in {
       enable = mkDefault true;
       settings = { PasswordAuthentication = mkDefault true; };
       #passwordAuthentication = mkDefault false;
-      settings = { PermitRootLogin = mkDefault true; };
+      settings = { PermitRootLogin = mkDefault "yes"; };
     };
     environment.etc = mkMerge (mapAttrsToList (name: pwd: {
       "NetworkManager/system-connections/${name}.nmconnection" = {
